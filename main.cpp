7b359beaf9d2ddf1723caa233aea79a1590e7225
QweIssLol <egorytub90@gmail.com>
2025-12-04 16:23:24 +0100

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/tut1.h>
-#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/tut2.h>
-#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/tut3.h>
-#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/tut4.h>
-#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/tut5.h>
+#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/asio_headers/tut1.h>
+#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/asio_headers/tut2.h>
+#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/asio_headers/tut3.h>
+#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/asio_headers/tut4.h>
+#include</Users/Root/source/repos/listenwthme/p_listenwthme/p_listenwthme/asio_headers/tut5.h>
 
 int main() {
 	tut5();
